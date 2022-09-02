4a7a538f3b42383d4e7849d1bfbbf9e98b219bbd
Liam <something.random.i.made.at.3am@gmail.com>
2022-09-02 18:47:06 +1200

    v1

--- index.js
+++ index.js
@@ -1,71 +1,80 @@
 var state = 0 // 0: Wating to start, 1: Running Test, 2: Test Finished
 var isGreen = false
-var startTime, endTime
+var output = ""
 var testsRan = 0
-var timeout = 0
 var testsToRun = 5
-var max = 5
-var min = 2
-var score = []
+var value = 0
+var length = 5
+var score = [],
+    incorrectAnwsers = []
 
-function turnGreen() {
-    startTime = performance.now()
-    isGreen = true
-    $("body").addClass("bg-green");
-    $("body").removeClass("bg-red");
-}
-
-function reset() {
-    state = 0
-    isGreen = false
+function randomBinary(binaryLength) {
+    binary = "";
+    for (let i = 0; i < binaryLength; ++i) {
+        binary += Math.floor(Math.random() * Math.floor(2));;
+    }
+    return binary;
 }
 
 $(document).ready(function() {
     $("#mainButton").on("click", function() {
         switch (state) {
             case 0:
-                $("body").addClass("bg-red");
-                $("body").removeClass("bg-green");
-                $("#title").text("Wait Until Green")
-                $("#info").text("Click any key when the screen turns green.")
-                $("#mainButton").text("Click Me!")
+                $("body").removeClass("bg-red")
+                $("body").removeClass("bg-green")
+                value = randomBinary(length)
+                $("#title").text(value)
+                $("#info").html("<input type='text' class='form-control' id='anwser' placeholder='Decimal Value'>")
+                $("#mainButton").text("Submit!")
                 state = 1
-                timeout = setTimeout(turnGreen, Math.floor(Math.random() * (max - min) + min) * 1000)
                 break;
             case 1:
-                if (isGreen) {
-                    endTime = performance.now()
-                    score[testsRan] = endTime - startTime
-                    $("#title").text(score[testsRan] + "ms")
-                    testsRan++
-                    $("#info").html("Click any key to continue. <b>" + testsRan + " / " + testsToRun + " </b>")
-                    if (testsRan == testsToRun) {
-                        $("#mainButton").text("Finish!")
-                        state = 2
-                    } else {
-                        $("#mainButton").text("Continue!")
-                        reset()
-                    }
+                testsRan++
 
+                anwser = parseInt(value, 2) == parseInt($("#anwser").val())
+                if (anwser) {
+                    $("body").addClass("bg-green")
+                    $("#title").text("Correct")
+                    $("#info").text(value + " == " + parseInt(value, 2))
                 } else {
-                    clearTimeout(timeout)
-                    $("#title").text("Whoa, Slow Down")
-                    $("#info").text("Wait until the screen turns green before clicking.")
-                    $("#mainButton").text("Try Again!")
-                    reset()
+                    $("body").addClass("bg-red")
+                    $("#title").text("Incorrect")
+                    incorrectAnwsers.push(`Question ${testsRan}: ${value} = ${parseInt(value, 2)}. Your anwser: ${$("#anwser").val()}`)
+                    $("#info").text("Anwser: " + parseInt(value, 2))
+                }
+                console.log(anwser);
+                score.push(anwser)
+                if (testsRan == testsToRun) {
+                    $("#mainButton").text("Finish!")
+                    state = 2
+                } else {
+                    $("#mainButton").text("Next!")
+                    state = 0
                 }
                 break;
             case 2:
                 state = 0
                 $("body").removeClass("bg-green")
-                $("#title").text("You Finished! avg: " + Math.round(score.reduce((a, b) => a + b) / score.length) + "ms")
-                $("#info").html("Min: " + Math.min.apply(null, score) + "ms & Max: " + Math.max.apply(null, score) + "ms <br>" + score.join("<br>"))
+                $("body").removeClass("bg-red")
+                $("#title").text("You Finished: " + Math.round(score.filter(value => value === true).length * 100 / score.length) + "% Correct")
+                $("#info").html(incorrectAnwsers.join("<br>"))
                 $("#mainButton").text("Again?")
+                testsRan = 0
                 break;
         }
     });
-    $('#timesToRun').on('input', function(e) {
-        testsToRun = parseInt($('#timesToRun').val()) || 5
+    length = parseInt($("#length").val()) || 5
+    testsToRun = parseInt($("#timesToRun").val()) || 5
+    $("#timesToRun").on("input", function(e) {
+        testsToRun = parseInt($("#timesToRun").val()) || 5
+    });
+    $("#length").on("input", function(e) {
+        length = parseInt($("#length").val()) || 5
+    });
+    $("body").keyup(function(e) {
+        if (e.which == 13) {
+            $("#mainButton").click()
+        }
     });
 
 });
