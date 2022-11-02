352cba42055e473bdd483ab1f143f2b0c5a625bf
Liam <something.random.i.made.at.3am@gmail.com>
2022-11-02 13:18:37 +1300

    bug fixed

--- index.js
+++ index.js
@@ -1,67 +1,86 @@
 var state = 0 // 0: Wating to start, 1: Running Test, 2: Test Finished
 var isGreen = false
+var binaryIsValid = false
 var output = ""
 var testsRan = 0
 var testsToRun = 5
 var value = 0
 var length = 5
+var lastValue = ""
 var score = [],
-    incorrectAnwsers = []
+    incorrectAnswers = []
 
 function randomBinary(binaryLength) {
-    binary = "";
-    for (let i = 0; i < binaryLength; ++i) {
-        binary += Math.floor(Math.random() * Math.floor(2));;
+    while (!binaryIsValid) {
+        binary = "";
+        for (let i = 0; i < binaryLength; ++i) {
+            binary += Math.floor(Math.random() * Math.floor(2));
+        }
+        console.log(`Last: ${lastValue} : New: ${binary}`)
+        if (lastValue != binary) binaryIsValid = true
     }
+    binaryIsValid = false
+    lastValue = binary
     return binary;
 }
 
-$(document).ready(function() {
-    $("#mainButton").on("click", function() {
-        switch (state) {
-            case 0:
-                $("body").removeClass("bg-red")
-                $("body").removeClass("bg-green")
-                value = randomBinary(length)
-                $("#title").text(value)
-                $("#info").html("<input type='text' class='form-control' id='anwser' placeholder='Decimal Value'>")
-                $("#mainButton").text("Submit!")
-                state = 1
-                break;
-            case 1:
-                testsRan++
+function click () {
+    switch (state) {
+        case 0:
+            $("#title").text("Learn Binary")
+            $("#info").html(`Click the button below to start. The test will use values: <input type='number' id='length' min='2' value='${length}' style='width:40px; background: none;border: none;color: white;''> bits long<br> And Will run: <input type='number' id='timesToRun' min='2' value='${testsToRun}' style='width:40px; background: none;border: none;color: white;'> Times`)
+            $("#mainButton").text("Start!")
+            incorrectAnswers, score, testsRan = 0
+            state=1
+            break;
+        case 1:
+            $("body").removeClass("bg-red")
+            $("body").removeClass("bg-green")
+            value = randomBinary(length)
+            $("#title").text(value)
+            $("#info").html("<input type='text' class='form-control' id='Answer' placeholder='Decimal Value'>")
+            $("#mainButton").text("Submit!")
+            state = 2
+            break;
+        case 2:
+            testsRan++
 
-                anwser = parseInt(value, 2) == parseInt($("#anwser").val())
-                if (anwser) {
-                    $("body").addClass("bg-green")
-                    $("#title").text("Correct")
-                    $("#info").text(value + " == " + parseInt(value, 2))
-                } else {
-                    $("body").addClass("bg-red")
-                    $("#title").text("Incorrect")
-                    incorrectAnwsers.push(`Question ${testsRan}: ${value} = ${parseInt(value, 2)}. Your anwser: ${$("#anwser").val()}`)
-                    $("#info").text("Anwser: " + parseInt(value, 2))
-                }
-                console.log(anwser);
-                score.push(anwser)
-                if (testsRan == testsToRun) {
-                    $("#mainButton").text("Finish!")
-                    state = 2
-                } else {
-                    $("#mainButton").text("Next!")
-                    state = 0
-                }
-                break;
-            case 2:
-                state = 0
-                $("body").removeClass("bg-green")
-                $("body").removeClass("bg-red")
-                $("#title").text("You Finished: " + Math.round(score.filter(value => value === true).length * 100 / score.length) + "% Correct")
-                $("#info").html(incorrectAnwsers.join("<br>"))
-                $("#mainButton").text("Again?")
-                testsRan = 0
-                break;
-        }
+            answer = parseInt(value, 2) == parseInt($("#Answer").val())
+            if (answer) {
+                $("body").addClass("bg-green")
+                $("#title").text("Correct")
+                $("#info").text(value + " == " + parseInt(value, 2))
+            } else {
+                $("body").addClass("bg-red")
+                $("#title").text("Incorrect")
+                incorrectAnswers.push(`Question ${testsRan}: ${value} = ${parseInt(value, 2)}. Your Answer: ${$("#Answer").val()}`)
+                $("#info").text("Answer: " + parseInt(value, 2))
+            }
+            score.push(answer)
+            if (testsRan == testsToRun) {
+                $("#mainButton").text("Finish!")
+                state = 3
+            } else {
+                $("#mainButton").text("Next!")
+                state = 1
+            }
+            break;
+        case 3:
+            state = 0
+            $("body").removeClass("bg-green")
+            $("body").removeClass("bg-red")
+            $("#title").text("You Finished: " + Math.round(score.filter(value => value === true).length * 100 / score.length) + "% Correct")
+            $("#info").html(incorrectAnswers.join("<br>"))
+            $("#mainButton").text("Again?")
+            testsRan = 0
+            break;
+    }
+}
+
+$(document).ready(function() {
+    click()
+    $("#mainButton").on("click", function() {
+        click()
     });
     length = parseInt($("#length").val()) || 5
     testsToRun = parseInt($("#timesToRun").val()) || 5
@@ -73,7 +92,7 @@ $(document).ready(function() {
     });
     $("body").keyup(function(e) {
         if (e.which == 13) {
-            $("#mainButton").click()
+            click()
         }
     });
 
