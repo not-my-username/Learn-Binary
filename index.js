39aaf48507e2fb48f57676056865a987b09d465a
Your Name <you@example.com>
2023-03-19 19:57:42 +1300

    Added Decimal To Binary Test

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var state = 0 // 0: Wating to start, 1: Running Test, 2: Test Finished
+var state = 0 // 0: Wating to start, 1 && 2: Running Test, 3: Test Finished
 var isGreen = false
 var binaryIsValid = false
 var output = ""
 var testsRan = 0
+var type = 0
 var testsToRun = 5
 var value = 0
 var length = 5
@@ -30,30 +31,42 @@ function click () {
             $("#title").text("Learn Binary")
             $("#info").html(`Click the button below to start. The test will use values: <input type='number' id='length' min='2' value='${length}' style='width:40px; background: none;border: none;color: white;''> bits long<br> And Will run: <input type='number' id='timesToRun' min='2' value='${testsToRun}' style='width:40px; background: none;border: none;color: white;'> Times`)
             $("#mainButton").text("Start!")
-            incorrectAnswers = []
-            score = []
-            testsRan = 0
-            state=1
-            $("#timesToRun").on("input", function(e) {
+            $("#switchBox").show()
+            $("#timesToRun").on("input", function() {
                 testsToRun = parseInt($("#timesToRun").val()) || 5
             });
-            $("#length").on("input", function(e) {
+            $("#length").on("input", function() {
                 length = parseInt($("#length").val()) || 5
             });
+            incorrectAnswers = []
+            score = []
+            state=1
             break;
         case 1:
             $("body").removeClass("bg-red")
             $("body").removeClass("bg-green")
             value = randomBinary(length)
-            $("#title").text(value)
-            $("#info").html("<input type='text' class='form-control' id='Answer' placeholder='Decimal Value'>")
+            if (type == 0) {
+                $("#title").text(value)
+                $("#info").html("<input type='text' class='form-control' id='Answer' placeholder='Decimal Value'>")
+            }else{
+                $("#title").text(parseInt(value, 2))
+                $("#info").html("<input type='text' class='form-control' id='Answer' placeholder='Binary Value'>")  
+            }
+            $("#info").focus()
             $("#mainButton").text("Submit!")
+            $("#switchBox").hide()
             state = 2
             break;
         case 2:
             testsRan++
-
-            answer = parseInt(value, 2) == parseInt($("#Answer").val())
+            if (type == 0) {
+                answer = parseInt(value, 2) == parseInt($("#Answer").val())
+            }else{
+                console.log("Your:" + ($("#Answer").val()>>>0).toString(10))
+                console.log("Anwser: " + parseInt(value));
+                answer = parseInt(value) == ($("#Answer").val()>>>0).toString(10); 
+            }
             if (answer) {
                 $("body").addClass("bg-green")
                 $("#title").text("Correct")
@@ -61,7 +74,11 @@ function click () {
             } else {
                 $("body").addClass("bg-red")
                 $("#title").text("Incorrect")
-                incorrectAnswers.push(`Question ${testsRan}: ${value} = ${parseInt(value, 2)}. Your Answer: ${$("#Answer").val()}`)
+                if (type = 0) {
+                    incorrectAnswers.push(`Question ${testsRan}: ${value} = ${parseInt(value, 2)}. Your Answer: ${$("#Answer").val()}`)
+                }else{
+                    incorrectAnswers.push(`Question ${testsRan}: ${parseInt(value, 2)} = ${value}. Your Answer: ${$("#Answer").val()}`)
+                }
                 $("#info").text("Answer: " + parseInt(value, 2))
             }
             score.push(answer)
@@ -95,5 +112,8 @@ $(document).ready(function() {
             click()
         }
     });
-
+    $("#switch").on("input", function (e) {
+        type = e.target.checked ? 1 : 0
+        $("#switchLabel").text(e.target.checked ? "Decimal To Binary" : "Binary To Decmial")
+    })
 });
